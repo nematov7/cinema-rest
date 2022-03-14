4487d25e391409dc934eeff308dc40c04b3d6798
NematovHojiakbar <hojiakbarnematov0501@gmail.com>
2022-03-14 15:27:01 +0500

    first commit

---
+++ src/main/java/uz/pdp/cinemarest/CinemaRestApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.cinemarest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaRestApplication.class, args);
+    }
+
+}
---
+++ src/main/java/uz/pdp/cinemarest/abs/AbsEntity.java
@@ -0,0 +1,12 @@
+package uz.pdp.cinemarest.abs;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
+public class AbsEntity {
+}
