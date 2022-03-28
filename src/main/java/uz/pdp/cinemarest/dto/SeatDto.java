5c66ac6f012df5c97bf1cbbb4aca0445898ce595
NematovHojiakbar <hojiakbarnematov0501@gmail.com>
2022-03-28 16:47:49 +0500

    director crud

---
+++ src/main/java/uz/pdp/cinemarest/dto/SeatDto.java
@@ -0,0 +1,14 @@
+package uz.pdp.cinemarest.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.PackagePrivate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@PackagePrivate
+public class SeatDto {
+    String name;
+}
--- src/main/java/uz/pdp/cinemarest/repository/GenreRepository.java
+++ src/main/java/uz/pdp/cinemarest/repository/GenreRepository.java
@@ -1,10 +1,21 @@
 package uz.pdp.cinemarest.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import uz.pdp.cinemarest.entity.Genre;
 import uz.pdp.cinemarest.projection.CustomGenre;
+import uz.pdp.cinemarest.projection.GenreProjection;
+
+import java.util.List;
 
 @RepositoryRestResource(path = "genre",collectionResourceRel = "list",excerptProjection = CustomGenre.class)
 public interface GenreRepository extends JpaRepository<Genre,Integer> {
+
+
+    @Query(nativeQuery = true,value = "select g.id ,g.name from movie_genres\n" +
+            "join genre g on movie_genres.genres_id = g.id\n" +
+            "where movie_id= :id")
+    List<GenreProjection> getGenresesByMovieId(Integer id);
+
 }
